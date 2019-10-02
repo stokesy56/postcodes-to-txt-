f7b845e26638d74d2f0a946721515ced480c3800
Rory Stokes <rstokes@spartaglobal.com>
2019-10-02 21:50:48 +0100

    made function to write postcode info to text file

--- functions.py
+++ functions.py
@@ -1,7 +1,15 @@
 import requests
+import json
 
 def postcode_info(postcode):
     post_codes = requests.get(f"http://api.postcodes.io/postcodes/{postcode}".lower().strip())
     post_code_dict = post_codes.json()
     return post_code_dict['result']
 
+def write_postcode_info_to_text(postcode):
+    try:
+        with open('postcode_info.txt', 'w') as outfile:
+            json.dump(postcode_info(postcode), outfile)
+
+    except FileNotFoundError:
+        print('File not found')
--- run.py
+++ run.py
@@ -1,3 +1,5 @@
 from functions import *
 
 print(postcode_info('WD3 8hy'))
+
+print(write_postcode_info_to_text('wd38hy'))
